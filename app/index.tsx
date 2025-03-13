b45780dd79428ca3a2dae4d9f993a0bf18bb3908
Jingshi Liu <jingshiliu07@gmail.com>
2025-03-12 20:26:56 -0400

    init commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className={"flex-1 justify-center items-center"}
+    >
+      <Text className={"text-light-200 text-5xl"}>Welcomessssqweqweqweds</Text>
+    </View>
+  );
+}
